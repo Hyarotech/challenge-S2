9b8fb4e7a23e3860eba23a55d74c5a375a1608e0
Jawad-Ahmindache <101027749+Jawad-Ahmindache@users.noreply.github.com>
2023-06-29 11:08:14 +0200

    Hotfix/model singleton and router reflection (#18)
    * Correction reflexion routeur
    
    La réflexion dans routeur pour invoquer Core\Request peut utiliser une classe enfant de Core\Requesr à partir d'un
    attribut de PHP 8
    
    * Modifications dans model
    
    * Remplacement de certain mot clé self par static afin d'appeler la classe enfant et non le parent
    * Connector DB Singleton
    * possibilité de donner un model à save()
    
    * suppression de page car pas finie
    
    Pour avoir uniquement les fix sur le model et le router
    
    * Delete PageController.php

--- www/core/Model.php
+++ www/core/Model.php
@@ -1,88 +1,92 @@
 <?php
-
 namespace Core;
 
-use PDO;
+use Exception;
 use ReflectionNamedType;
 
 abstract class Model
 {
-
-    protected PDO $pdo;
     protected string $table;
     protected ?array $fillable = null;
-    protected static $singleton;
 
     public function __construct()
     {
 
-        //Mettre en place un SINGLETON
-        try {
-            $this->pdo = new PDO("pgsql:host=" . env("DB_HOST") . ";port=" . env("DB_PORT") . ";dbname=" . env("DB_DATABASE"), env("DB_USERNAME"), env("DB_PASSWORD"));
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        } catch (\Exception $e) {
-            die("Erreur SQL : " . $e->getMessage());
-        }
         $classExploded = explode("\\", get_called_class());
         $this->table = end($classExploded);
+
         $this->table = env("DB_SCHEMA", "public") . "." . $this->table;
     }
 
-    public static function getInstance(): static
+    public static function hydrate(array $data = []): static
     {
-        if (!self::$singleton) {
-            self::$singleton = new static();
-        }
-        return self::$singleton;
-    }
-
-    public static function hydrate(array $data): Model
-    {
-        $instance = self::getInstance();
+        $instance = new static();
         $reflection = new \ReflectionClass($instance);
-        foreach($data as $key => $value){
+        foreach ($data as $key => $value) {
             $property = toCamelCase($key);
-            if($reflection->hasProperty($property)){
+            if ($reflection->hasProperty($property)) {
                 $property = $reflection->getProperty($property);
                 $propertyType = $property->getType();
                 $property = $property->getName();
-                if($propertyType instanceof ReflectionNamedType && $propertyType->getName() === "DateTime"){
+                if ($propertyType instanceof ReflectionNamedType && $propertyType->getName() === "DateTime") {
                     try {
                         $instance->$property = new \DateTime($value);
                     } catch (\Exception $e) {
                         $instance->$property = null;
                     }
-                } else{
+                } else {
                     $instance->$property = $value;
                 }
             }
         }
         $result = clone $instance;
         $toRemove = get_class_vars(get_class());
-        foreach ($toRemove as $key=> $value){
-            if($key !== "singleton"){
+        foreach ($toRemove as $key => $value) {
+            if ($key !== "singleton") {
                 unset($result->$key);
             }
         }
         return $result;
     }
 
-    public static function save(array $data): void
+    public static function save(Model|array $data = []): bool
     {
-        $instance = self::getInstance();
-        if ($instance->fillable) {
-            $data = array_intersect_key($data, array_flip($instance->fillable));
+        $dbConnector = DBConnector::getInstance();
+        $pdo = $dbConnector->getPDO();
+
+        $fillable = new static();
+        $fillable = $fillable->fillable;
+        if ($fillable && is_array($data)) {
+            $instance = new static();
+            $data = array_intersect_key($data, array_flip($fillable));
+        }
+        else if ($data instanceof Model) {
+            foreach ($fillable as $value) {
+                $valueCamel = toCamelCase($value);
+                // Vérifier si la méthode getValeurFilled existe
+                $getterMethod = 'get' . ucfirst($valueCamel);
+                
+                if (method_exists($data, $getterMethod))
+                    $listData[$value] = $data->{$getterMethod}();
+                
+                
+            }
+            $instance = $data;    
+            $data = $listData;
         }
         $columns = array_keys($data);
-        $queryPrepared = $instance->pdo->prepare("INSERT INTO " . $instance->table . " (" . implode(",", $columns) . ") 
+        
+        $queryPrepared = $pdo->prepare("INSERT INTO " . $instance->table . " (" . implode(",", $columns) . ") 
                             VALUES (:" . implode(",:", $columns) . ")");
-        $queryPrepared->execute($data);
+        return  $queryPrepared->execute($data);
     }
 
     public static function update(string $id, array $data): void
     {
-        $instance = self::getInstance();
+        $dbConnector = DBConnector::getInstance();
+        $pdo = $dbConnector->getPDO();
+
+        $instance = new static();
         if ($instance->fillable) {
             $data = array_intersect_key($data, array_flip($instance->fillable));
         }
@@ -94,40 +98,53 @@ abstract class Model
             $columns[] = $key . "=:" . $key;
         }
         $data["id"] = $id;
-        $queryPrepared = $instance->pdo->prepare("UPDATE " . $instance->table . " SET " . implode(",", $columns) . " WHERE id=:id");
+        $queryPrepared = $pdo->prepare("UPDATE " . $instance->table . " SET " . implode(",", $columns) . " WHERE id=:id");
         $queryPrepared->execute($data);
     }
+
     public static function findBy(string $column, string $value): ?Model
     {
-        $instance = self::getInstance();
-        $queryPrepared = $instance->pdo->prepare("SELECT * FROM " . $instance->table . " WHERE " . $column . " = :value");
+
+        $dbConnector = DBConnector::getInstance();
+        $pdo = $dbConnector->getPDO();
+
+        $instance = new static();
+
+        $queryPrepared = $pdo->prepare("SELECT * FROM " . $instance->table . " WHERE " . $column . " = :value");
         $queryPrepared->bindValue(":value", $value);
         $queryPrepared->execute();
-        $result =  $queryPrepared->fetch();
-        if($result){
-            return self::hydrate($result);
+        
+        $result = $queryPrepared->fetch();
+        if ($result) {
+            return static::hydrate($result);
         }
         return null;
     }
 
     public static function findAll(): array
     {
-        $instance = self::getInstance();
-        $results =  $instance->pdo->query("SELECT * FROM " . $instance->table)->fetchAll();
+        $dbConnector = DBConnector::getInstance();
+        $pdo = $dbConnector->getPDO();
+
+        $instance = new static();
+        $results = $pdo->query("SELECT * FROM " . $instance->table)->fetchAll();
         return array_map(function ($result) {
-            return self::hydrate($result);
+            return static::hydrate($result);
         }, $results);
     }
 
     public static function findOne(int $id): ?Model
     {
-        $instance = self::getInstance();
-        $queryPrepared = $instance->pdo->prepare("SELECT * FROM " . $instance->table . " WHERE id = :id");
+        $dbConnector = DBConnector::getInstance();
+        $pdo = $dbConnector->getPDO();
+
+        $instance = new static();
+        $queryPrepared = $pdo->prepare("SELECT * FROM " . $instance->table . " WHERE id = :id");
         $queryPrepared->bindValue(":id", $id);
         $queryPrepared->execute();
-        $result =  $queryPrepared->fetch();
-        if($result){
-            return self::hydrate($result);
+        $result = $queryPrepared->fetch();
+        if ($result) {
+            return static::hydrate($result);
         }
         return null;
     }
