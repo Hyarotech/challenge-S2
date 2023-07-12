7060129ee198407a6e76e2043c5af673a073f873
Houmame LAZAR <houms937@gmail.com>
2023-07-12 07:37:27 +0200

    :construction: ajout framework_js

--- www/public/assets/js/app.js
+++ www/public/assets/js/app.js
@@ -1,5 +1,33 @@
-import Navbar from "./Main/Navbar.js";
-import SideBar from "./Main/SideBar.js";
+import createElement from './vdom/createElement'
+import render from './vdom/render';
+import mount from './vdom/mount';
+import diff from './vdom/diff';
 
-$(document).ready(function(){
+const createVApp = (count) => createElement('div', {
+    attrs: {
+        id: 'app',
+        dataCount: count,
+    },
+    children: [
+        createElement('input'),
+        String(count),
+        ...Array.from({ length: count }, () => createElement('img', {
+            attrs: {
+                src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif',
+            },
+        })),
+    ],
 });
+
+let count = 0;
+let vApp = createVApp(count);
+const $app = render(vApp);
+
+let $rootEl = mount($app, document.getElementById('app'));
+
+setInterval(() => {
+    const vNewApp = createVApp(Math.floor(Math.random() * 10));
+    const patch = diff(vApp, vNewApp);
+    $rootEl = patch($rootEl);
+    vApp = vNewApp;
+}, 1000);
--- www/public/index.php
+++ www/public/index.php
@@ -2,9 +2,14 @@
 
 define("ROOT", dirname(__DIR__));
 require ROOT . "/vendor/autoload.php";
+
 use Core\Router;
 use Core\App;
 
+if (!file_exists(ROOT . "/.env")) {
+    header("Location: /install.php");
+    exit;
+}
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
