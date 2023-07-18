d674d39927dd90d17bdbe2433bc3f164834c09e6
Stephane LAZAREVIC <stephanelazarevic042@outlook.com>
2023-07-18 09:59:46 +0200

    save Controller

--- www/app/Controllers/User/UserControllerApi.php
+++ www/app/Controllers/User/UserControllerApi.php
@@ -7,7 +7,7 @@ class UserControllerApi implements ControllerApi
 
     public function read(GetUserRequest $request)
     {
-      
+        $this->request = $request;
     }
     
     public function readAll(){
@@ -16,17 +16,19 @@ class UserControllerApi implements ControllerApi
 
     public function create(CreateUserRequest $request)
     {
-        
+        $this->request = $request;
+        $user = new User($request->getData());
+        $user->save();
     }
 
     public function update(UpdateUserRequest $request)
     {
-        
+        $this->request = $request;
     }
 
     public function delete(DeleteUserRequest $request)
     {
-      
+        $this->request = $request;
     }
 
 }
