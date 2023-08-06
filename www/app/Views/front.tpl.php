ae9343e4294588147022654a665858963160789b
Jawad-Ahmindache <jawad.ahmindache@gmail.com>
2023-08-06 14:57:07 +0200

    Ajout des attributs meta et title dynamique dans le tpl front

--- www/app/Views/front.tpl.php
+++ www/app/Views/front.tpl.php
@@ -13,6 +13,11 @@ getJS();
     src="https://code.jquery.com/jquery-3.7.0.min.js"
     integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
     crossorigin="anonymous"></script>
+
+        <script defer
+    src="https://code.jquery.com/jquery-3.7.0.min.js"
+    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
+    crossorigin="anonymous"></script>
   
     <script type="module" defer src="<?= getJS() ?>"></script>
 
