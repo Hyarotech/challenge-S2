02784bc49b9e651b62a5ae871d8503089f17a3b7
Jawad-Ahmindache <jawad.ahmindache@gmail.com>
2023-08-18 12:32:06 +0200

    Correction espace sur textarea dans page

--- www/app/Views/Page/pageSetting.view.php
+++ www/app/Views/Page/pageSetting.view.php
@@ -24,13 +24,11 @@
       <span class="label-text-alt text-red-500 col-span-6">
                             <?= \Core\Session::getError("title") ?>
                         </span>
-      <input name="slug" readonly="text" placeholder="Slug" value="<?= $slug ?? '' ?>" class="col-span-6 input input-bordered" />
+      <input name="slug" placeholder="Slug" value="<?= $slug ?? '' ?>" class="col-span-6 input input-bordered" />
       <span class="label-text-alt text-red-500 col-span-6">
                             <?= \Core\Session::getError("slug") ?>
                         </span>
-      <textarea name="description" class="min-h-[200px] col-span-6 textarea textarea-bordered" placeholder="Description">
-        <?= $description ?? '' ?>
-      </textarea>
+      <textarea name="description" class="min-h-[200px] col-span-6 textarea textarea-bordered"><?= $description ?? '' ?></textarea>
       <span class="label-text-alt text-red-500 col-span-6">
                             <?= \Core\Session::getError("description") ?>
                         </span>
