13a2c3e6d68311451097b8aa408200af40a199c3
IMAMI Hajar <hajarima7@gmail.com>
2023-09-12 23:59:20 +0200

    :sparkles: sitemap + profile

--- www/app/Views/Nav/navbar.view.php
+++ www/app/Views/Nav/navbar.view.php
@@ -91,25 +91,45 @@ function renderMenuMobile(string $menuItems) {
     </ul>
   </div>
   <div class="navbar-end">
-  <div class="dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                        </div>
-                    </label>
-                   
-                    <ul tabindex="0"
-                        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <a class="justify-between">
-                                Profile
-                                <span class="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a href="<?= \Core\Router::generateRoute('security.logout'); ?>">Logout</a></li>
-                    </ul>
-                </div>
+      <?php if (\Core\Session::has("user")): ?>
+          <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                        <span class="block w-10 rounded-full">
+                            <img src="https://source.unsplash.com/100x100/?portrait" class="rounded-full w-28 "
+                                 alt="profile picture" srcset="">
+                        </span>
+              </label>
+
+              <ul tabindex="0"
+                  class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                  <li>
+                      <a href="<?= \Core\Router::generateRoute("security.profile") ?>" class="justify-between">
+                          Profile
+                      </a>
+                  </li>
+                  <li><a href="<?= \Core\Router::generateRoute('security.logout'); ?>">Logout</a></li>
+              </ul>
+          </div>
+      <?php else: ?>
+
+          <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-primary btn-circle avatar">
+                        <span class="block w-10 rounded-full">
+                            <i class="fa fa-user"></i>
+                        </span>
+              </label>
+
+              <ul tabindex="0"
+                  class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                  <li>
+                      <a href="<?= \Core\Router::generateRoute("security.login")?>" class="justify-between">
+                          Login
+                      </a>
+                  </li>
+                  <li><a href="<?= \Core\Router::generateRoute('security.register'); ?>">Register</a></li>
+              </ul>
+          </div>
+      <?php endif; ?>
 
   </div>
             
